c9d92066cf1ac09850335e2458b5c9f1d12e7195
han <hhw12125@gmail.com>
2017-05-06 16:59:30 +0800

    Web Forms

--- app/views.py
+++ app/views.py
@@ -4,8 +4,10 @@
 # is mapped to one or more request URLs.
 
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -20,7 +22,24 @@ def index():
 		'author':{'nickname':'Susan'},
 		'body':'The Avengers movie was so cool!'
 	}]
-	return render_template('index.html',
+	return render_template(
+		'index.html',
 		title = 'Home',
 		user = user,
 		posts = posts)
+
+@app.route('/login',methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login required for Openid="%s",remember_me=%s' % 
+			(form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template(
+		'login.html',
+		title = 'login',
+		form = form,
+		providers = app.config['OPENID_PROVIDERS'])
+
+
+
