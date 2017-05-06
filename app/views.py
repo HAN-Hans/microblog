abb3ee879d5de7ddf43882e0ed317f9365366d5f
han <hhw12125@gmail.com>
2017-05-06 14:26:03 +0800

    Templates

--- app/views.py
+++ app/views.py
@@ -3,9 +3,24 @@
 # are written as Python functions. Each view function 
 # is mapped to one or more request URLs.
 
+
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello Flask World'
+	user = {'nickname':'Justin'}	# fake user
+	posts = [
+	{
+		'author':{'nickname':'John'},
+		'body':'Beautiful day in Portland!'
+	},
+	{
+		'author':{'nickname':'Susan'},
+		'body':'The Avengers movie was so cool!'
+	}]
+	return render_template('index.html',
+		title = 'Home',
+		user = user,
+		posts = posts)
