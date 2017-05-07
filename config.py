22366c92e5fc854979c4e673bf25028096968f66
han <hhw12125@gmail.com>
2017-05-07 17:09:26 +0800

    Profile Page And Avatars

--- config.py
+++ config.py
@@ -14,7 +14,9 @@ OPENID_PROVIDERS = [
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+    # {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+    ]
 
 
 # 839: FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds 
